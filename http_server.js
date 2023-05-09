31cbe9c1ed14bf3d5d5083693125dddb6cf64a74
Ahmad Karimi <zikman@gmail.com>
2023-05-09 01:38:54 -0400

    update to deploy

--- http_server.js
+++ http_server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app     = express();
-const low     = require('lowdb');
-const fs      = require('lowdb/adapters/FileSync');
+const app = express();
+const low = require('lowdb');
+const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
-const db      = low(adapter);
-const cors    = require('cors');
+const db = low(adapter);
+const cors = require('cors');
 const { faker } = require('@faker-js/faker');
 
 // allow cross-origin resource sharing (CORS)
@@ -20,41 +20,37 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // init the data store
-db.defaults({ users: []}).write();
+db.defaults({ users: [] }).write();
+
+let port = process.env.PORT || 3000;
 
 // return all users
-app.get('/data', function(req, res){     
-    res.send(db.get('users').value());
-});
-
-// post route
-app.post('/test', function(req, res){
-    console.log(req.body.username, req.body.password);
-    res.send(req.body.username + " " + req.body.password)
+app.get('/data', function (req, res) {
+  res.send(db.get('users').value());
 });
 
 // add user
-app.post('/add', function(req, res){
-    var user = {
-        'name'          : req.body.name,
-        'dob'           : req.body.dob,
-        'email'         : req.body.email,
-        'username'      : req.body.username,
-        'password'      : req.body.password,
-        'phone'         : req.body.phone,
-        'streetaddress' : req.body.streetaddress,
-        'citystatezip'  : req.body.citystatezip,
-        'latitude'      : req.body.latitude,
-        'longitude'     : req.body.longitude,
-        'avatar'        : faker.internet.avatar() 
-    }
-    db.get('users').push(user).write();
-    console.log(db.get('users').value());
-    res.send(db.get('users').value());
+app.post('/add', function (req, res) {
+  var user = {
+    name: req.body.name,
+    dob: req.body.dob,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    phone: req.body.phone,
+    streetaddress: req.body.streetaddress,
+    citystatezip: req.body.citystatezip,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    avatar: faker.internet.avatar(),
+  };
+  db.get('users').push(user).write();
+  console.log(db.get('users').value());
+  res.send(db.get('users').value());
 });
 
 // start server
 // -----------------------
-app.listen(3000, function(){
-    console.log('Running on port 3000!')
-})
+app.listen(port, function () {
+  console.log(`Running on port ${port}`);
+});
